510d40c5a654f1ca0ffea8f575b9cfe53349c522
Suryahadiningrat <rifkysuryakusuma@floothink.com>
2025-09-08 16:36:53 +0700

    update

--- app/Views/admin/siswa/index.php
+++ app/Views/admin/siswa/index.php
@@ -30,67 +30,6 @@
             <h6 class="m-0 font-weight-bold text-primary">Daftar Siswa</h6>
         </div>
         <div class="card-body">
-            <!-- Filter Section -->
-            <div class="row mb-3">
-                <div class="col-md-3">
-                    <label for="filterKelas" class="form-label">Filter Kelas</label>
-                    <select id="filterKelas" class="form-select form-select-sm">
-                        <option value="">Semua Kelas</option>
-                        <?php
-                        $kelasOptions = [];
-                        foreach ($siswa ?? [] as $siswa_item) {
-                            if (!empty($siswa_item['nama_kelas']) && !in_array($siswa_item['nama_kelas'], $kelasOptions)) {
-                                $kelasOptions[] = $siswa_item['nama_kelas'];
-                            }
-                        }
-                        sort($kelasOptions);
-                        foreach ($kelasOptions as $kelas): ?>
-                            <option value="<?= $kelas ?>"><?= $kelas ?></option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-                <div class="col-md-3">
-                    <label for="filterJurusan" class="form-label">Filter Jurusan</label>
-                    <select id="filterJurusan" class="form-select form-select-sm">
-                        <option value="">Semua Jurusan</option>
-                        <?php
-                        $jurusanOptions = [];
-                        foreach ($siswa ?? [] as $siswa_item) {
-                            if (!empty($siswa_item['nama_jurusan']) && !in_array($siswa_item['nama_jurusan'], $jurusanOptions)) {
-                                $jurusanOptions[] = $siswa_item['nama_jurusan'];
-                            }
-                        }
-                        sort($jurusanOptions);
-                        foreach ($jurusanOptions as $jurusan): ?>
-                            <option value="<?= $jurusan ?>"><?= $jurusan ?></option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-                <div class="col-md-3">
-                    <label for="filterJenisKelamin" class="form-label">Filter Jenis Kelamin</label>
-                    <select id="filterJenisKelamin" class="form-select form-select-sm">
-                        <option value="">Semua</option>
-                        <option value="L">Laki-laki</option>
-                        <option value="P">Perempuan</option>
-                    </select>
-                </div>
-                <div class="col-md-3">
-                    <label for="filterStatus" class="form-label">Filter Status</label>
-                    <select id="filterStatus" class="form-select form-select-sm">
-                        <option value="">Semua Status</option>
-                        <option value="1">Aktif</option>
-                        <option value="0">Nonaktif</option>
-                    </select>
-                </div>
-            </div>
-            <div class="row mb-3">
-                <div class="col-md-12">
-                    <button type="button" class="btn btn-secondary btn-sm" id="resetFilter">
-                        <i class="fas fa-sync-alt"></i> Reset Filter
-                    </button>
-                </div>
-            </div>
-            
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
@@ -109,8 +48,7 @@
                     <tbody>
                         <?php $no = 1; ?>
                         <?php foreach ($siswa ?? [] as $siswa_item): ?>
-                            <tr data-jenis-kelamin="<?= $siswa_item['jenis_kelamin'] ?? '' ?>" 
-                                data-status="<?= ($siswa_item['is_active'] ?? false) ? '1' : '0' ?>">
+                            <tr>
                                 <td><?= $no++ ?></td>
                                 <td><?= $siswa_item['nis'] ?? '' ?></td>
                                 <td><?= $siswa_item['full_name'] ?? '' ?></td>
@@ -168,7 +106,7 @@
 
 <script>
 $(document).ready(function() {
-    var table = $('#dataTable').DataTable({
+    $('#dataTable').DataTable({
         responsive: true,
         language: {
             url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/id.json'
@@ -180,55 +118,6 @@ $(document).ready(function() {
                 searchable: false
             }
         ]
-    });
-
-    // Filter functions
-    $('#filterKelas').on('change', function() {
-        var selectedKelas = this.value;
-        if (selectedKelas === '') {
-            table.column(5).search('').draw();
-        } else {
-            table.column(5).search('^' + selectedKelas + '$', true, false).draw();
-        }
-    });
-
-    $('#filterJurusan').on('change', function() {
-        var selectedJurusan = this.value;
-        if (selectedJurusan === '') {
-            table.column(6).search('').draw();
-        } else {
-            table.column(6).search('^' + selectedJurusan + '$', true, false).draw();
-        }
-    });
-
-    $('#filterJenisKelamin').on('change', function() {
-        var selectedJK = this.value;
-        if (selectedJK === '') {
-            table.column(4).search('').draw();
-        } else {
-            var searchText = selectedJK === 'L' ? 'Laki-laki' : 'Perempuan';
-            table.column(4).search(searchText).draw();
-        }
-    });
-
-    $('#filterStatus').on('change', function() {
-        var selectedStatus = this.value;
-        if (selectedStatus === '') {
-            table.column(7).search('').draw();
-        } else {
-            var searchText = selectedStatus === '1' ? 'Aktif' : 'Nonaktif';
-            table.column(7).search(searchText).draw();
-        }
-    });
-
-    // Reset filter
-    $('#resetFilter').on('click', function() {
-        $('#filterKelas').val('');
-        $('#filterJurusan').val('');
-        $('#filterJenisKelamin').val('');
-        $('#filterStatus').val('');
-        
-        table.search('').columns().search('').draw();
     });
 });
 
@@ -249,4 +138,4 @@ function deleteSiswa(id, nama) {
     });
 }
 </script>
-<?= $this->endSection() ?>
+<?= $this->endSection() ?> 
